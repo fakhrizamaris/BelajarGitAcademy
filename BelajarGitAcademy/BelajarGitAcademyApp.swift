0c0b2326f86dc637ffe29170c81303a5c649be74
fakhrizamaris <djamarisfakhri@gmail.com>
2026-06-04 13:52:36 +0700

    Initial Commit

---
+++ BelajarGitAcademy/BelajarGitAcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BelajarGitAcademyApp.swift
+//  BelajarGitAcademy
+//
+//  Created by Fakhri Djamaris on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct BelajarGitAcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BelajarGitAcademy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BelajarGitAcademy
+//
+//  Created by Fakhri Djamaris on 04/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
