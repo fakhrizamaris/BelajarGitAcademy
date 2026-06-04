be40efd4cc916395aa11a75073190dbeabb808ba
fakhrizamaris <djamarisfakhri@gmail.com>
2026-06-04 16:38:07 +0700

    update text

--- BelajarGitAcademy/ContentView.swift
+++ BelajarGitAcademy/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Hello, fakhri")
-            Text("Hello, gess!")
+            Text("Hello, fakhri again!")
                 .foregroundStyle(.cyan)
                 .fontWeight(.black)
                 .frame(maxWidth: .random(in: 0..<300), maxHeight: .random(in: 0..<300))
